7dd6ad9612c92cc45a0263d811d066a364fc6403
Ali Can Tellioğlu <alicantellioglu@gmail.com>
2022-08-25 11:29:53 +0300

    Create example.php

---
+++ example.php
@@ -0,0 +1,16 @@
+<?php
+
+// construct library
+$canBusDecoder =  new CustomCANFrameParser ();
+
+//dbc file in converted json type
+$canBusDecoder>setDBCFile( "DBCFileInJsonFormat.json" );
+
+//can id 
+$canBusDecoder->setCANID("12CD10");
+
+//can data frame.
+$canBusDecoder->setCANMessage("01-a3-00-00-00-00-00-00");
+
+// dump extracted signal.
+var_dump( $canBusDecoder->decodeCANFrame() );
