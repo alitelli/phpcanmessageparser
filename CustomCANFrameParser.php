4aea7269db7c2bf9b9ca77c05eae9a587c0cec51
Ali Can Tellioğlu <alicantellioglu@gmail.com>
2023-08-11 11:13:54 +0300

    Added new function for hex to bin
    Problem with base_convert function

--- CustomCANFrameParser.php
+++ CustomCANFrameParser.php
@@ -30,9 +30,10 @@ class CustomCANFrameParser
 
     public function decodeCANFrame()
     {
+
         $this->_CANIDdec = hexdec($this->_CANID);
         foreach ($this->_dbcData->params as $canMessagesInDBC) {
-            if ($canMessagesInDBC->canId == $this->_CANIDdec) {
+            if ($canMessagesInDBC->canId == $this->_CANIDdec or $canMessagesInDBC->canId == $this->_CANIDdec+2147483648) {
                 $this->_canMessagesContent = $canMessagesInDBC;
                 $this->_CANMessage = strrev($this->_CANMessage->binnary);
                 foreach ($canMessagesInDBC->signals as $signal) {
@@ -60,37 +61,59 @@ class CustomCANFrameParser
                         $this->_CANID;
                     $this->_decodedSignals->{$signal->name}->SignalName =
                          $signal->name;
-                        
-                        
+                    $this->_decodedSignals->{$signal->name}->CalculatedValue =
+                         $this->_decodedSignals->{$signal->name}->value * $signal->factor  + $signal->offset;
+
+
                 }
                 break;
             }
         }
         unset($this->_CANID);
         unset($this->_CANMessage);
-        
+
         return $this->_decodedSignals;
     }
+
+
+    public function hex2binConv($hex) {
+        $table = array('0000', '0001', '0010', '0011',
+                    '0100', '0101', '0110', '0111',
+                    '1000', '1001', 'a' => '1010', 'b' => '1011',
+                    'c' => '1100', 'd' => '1101', 'e' => '1110',
+                    'f' => '1111');
+        $bin = '';
+
+        for($i = 0; $i < strlen($hex); $i++) {
+            $bin .= $table[strtolower(substr($hex, $i, 1))];
+        }
+
+        return $bin;
+    }
+
+    /**
+        Message formatter function
+        Input : { "10ff00f0": "1-0-0-0-0-0-0-0-" }
+        Input : { "10ff00f0": "1-0-0-0-0-0-0-0" }
+        Byte divider here is '-'.
+    **/
 
     public function formatCANFrame($Message)
     {
         $msg_bytes = explode("-", $Message);
-        $exp_element_c = is_countable($msg_bytes) ? count($msg_bytes) : 8 ;
-        if( $exp_element_c == 9) array_pop($msg_bytes);
+
         foreach ($msg_bytes as $c_msg_bytes) {
             $msg_bytes_e[] = strtoupper(
                 str_pad($c_msg_bytes, 2, "0", STR_PAD_LEFT)
             );
         }
+
         $msg_in_hex = implode("", array_reverse($msg_bytes_e));
         return (object) [
             "hex" => $msg_in_hex,
-            "binnary" => str_pad(
-                base_convert($msg_in_hex, 16, 2),
-                64,
-                "0",
-                STR_PAD_LEFT
-            ),
+            "binnary" =>$this->hex2binConv($msg_in_hex),
         ];
     }
+
+
 }
